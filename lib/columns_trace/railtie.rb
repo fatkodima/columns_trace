848b548b00c0e060712f83d133db7d9a2ac5e91a
fatkodima <fatkodima123@gmail.com>
2024-09-05 20:49:57 +0300

    Do not override custom logger in railtie

--- lib/columns_trace/railtie.rb
+++ lib/columns_trace/railtie.rb
@@ -7,7 +7,7 @@ module ColumnsTrace
       ColumnsTrace.backtrace_cleaner = Rails.backtrace_cleaner
 
       logger = ActiveSupport::Logger.new(Rails.root.join("log", "columns_trace.log"))
-      ColumnsTrace.reporter = LogReporter.new(logger)
+      ColumnsTrace.reporter ||= LogReporter.new(logger)
     end
   end
 end
